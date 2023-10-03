3baf7c3102d29bc02219c5ff378a4cf1707194f0
Алексей Пермяков <aapermy8@mts.ru>
2023-10-03 17:36:56 +0400

    Добавлен bootstrap

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'wares'
+    'wares',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
